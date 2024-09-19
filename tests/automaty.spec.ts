fa217fd0ab8fe3b1a176a0224c5cad622a0f4a81
chrisvasqm <christianvasquezmelo@gmail.com>
2024-09-18 23:44:35 -0400

    Add login alert test

--- tests/automaty.spec.ts
+++ tests/automaty.spec.ts
@@ -2,15 +2,26 @@ import {test, expect} from '@playwright/test';
 
 test.describe('Automaty', () => {
   test.describe('Login', () => {
-    test('should login with valid credentials', async ({page}) => {
+    test.beforeEach(async ({page}) => {
       await page.goto('https://automaty-gd3cb.ondigitalocean.app/');
+    });
 
+    test('should login with valid credentials', async ({page}) => {
       await page.getByLabel('Username', {exact: true}).fill('admin');
       await page.getByLabel('Password', {exact: true}).fill('admin');
       await page.getByRole('button', {name: 'Sign In'}).click();
 
-      const welcomeHeading = page.getByRole('heading', {name: 'Welcome back'});
-      await expect(welcomeHeading).toBeVisible({timeout: 6000});
+      const heading = page.getByRole('heading', {name: 'Welcome back'});
+      await expect(heading).toBeVisible({timeout: 6000});
+    });
+
+    test('should not login with invalid credentials', async ({page}) => {
+      await page.getByLabel('Username', {exact: true}).fill('admin');
+      await page.getByLabel('Password', {exact: true}).fill('admin123');
+      await page.getByRole('button', {name: 'Sign In'}).click();
+
+      const alert = page.getByText(/Invalid/i);
+      await expect(alert).toBeVisible();
     });
   });
 });
