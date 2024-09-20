c931d1e11f4984cc41c39bff81a328c2a97d53ad
chrisvasqm <christianvasquezmelo@gmail.com>
2024-09-20 13:07:01 -0400

    Remove unused page instances

--- tests/automaty.spec.ts
+++ tests/automaty.spec.ts
@@ -10,13 +10,13 @@ test.describe('Automaty', () => {
       login.goto();
     });
 
-    test('should login with valid credentials', async ({page}) => {
+    test('should login with valid credentials', async () => {
       login.signIn('admin', 'admin');
 
       await expect(login.welcomeHeading).toBeVisible({timeout: 6000});
     });
 
-    test('should not login with invalid credentials', async ({page}) => {
+    test('should not login with invalid credentials', async () => {
       login.signIn('admin', 'admin123');
 
       await expect(login.alert).toBeVisible();
